ec631bdb22bad5023c0dfffe44e06818274eeeb8
Ricard Villalba <ricardax@gmail.com>
2022-07-30 13:43:45 +0200

    lab done

--- my-first-project/src/App.vue
+++ my-first-project/src/App.vue
@@ -1,11 +1,14 @@
 <template>
   <nav>
     <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/about">About</router-link> |
+    <router-link to="/extra">Extra</router-link>
   </nav>
   <router-view/>
 </template>
+<script>
 
+</script>
 <style>
 #app {
   font-family: Helvetica, Arial, sans-serif;
---
+++ my-first-project/src/components/ToDoItem.vue
@@ -0,0 +1,32 @@
+<template>
+<div>
+  <ul v-for="item in array">
+    <li>{{ item }}</li>
+  </ul>
+  <button @click="helloWorld">click me to print hello world</button>
+</div>
+
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      array: [1, 2, 3, 4],
+    };
+  },
+methods: {
+  helloWorld () {
+    console.log('hello world');
+  }
+}
+
+
+
+
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
